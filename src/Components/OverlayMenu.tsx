d497b5e8e807cc8f4b5c725f55e39020b62bf511
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-07-26 15:28:38 +0200

    menu axios

--- src/Components/OverlayMenu.tsx
+++ src/Components/OverlayMenu.tsx
@@ -1,20 +1,55 @@
 import { FC, useEffect, useState } from 'react';
 import axios from "axios";
 
-const OverlayMenu: FC = () => {
-    const [ menuData, setMenuData ] = useState< {} | null >(null)
+type MenuObject = {
+    ID: number,
+    menu_order: number,
+    title: string,
+    slug: string,
+    url: string,
+    target: string,
+    description: string,
+    classes: string[]
+    menu_item_parent: string
+}
+type Menu = [ MenuObject ];
 
-    useEffect( () => {
-        axios.get("https://btg-communication.fr/wp-json/better-rest-endpoints/v1/menus/principal").then( res => setMenuData(res.data) )
-        console.log(menuData)
-    }, [])
+const OverlayMenu: FC = () => {
+    const [ menuData, setMenuData ] = useState<Menu | null>( null )
+    const [mainMenu, setMainMenu] = useState<Menu | null>(null)
+    const [subMenu, setSubMenu] = useState<Menu | null>(null)
+
+    const handleData = async () => {
+        await axios.get( "https://btg-communication.fr/wp-json/better-rest-endpoints/v1/menus/principal" ).then( res => {
+            let tempMainMenu = [];
+            let tempSubMenu = [];
+            res.data.map( (item: MenuObject) => {
+                if( item.menu_item_parent.length < 1 ) {
+                    tempMainMenu.push(item)
+                } else {
+                    tempSubMenu.push(item)
+                }
+            } )
+        } )
+    }
+
+    useEffect(  () => {
+        handleData()
+    }, [] )
 
     return (
         <div id="overlay-menu">
             <div className="content">
                 <div className="list-nav">
                     <nav>
-                        <ul className="menu-principal"></ul>
+                        <ul className="menu-principal">
+                            { menuData && menuData.map( menuItem => (
+                                    <li key={ menuItem.ID } className="menu-item">
+                                        { menuItem.title }
+                                    </li>
+                                )
+                            ) }
+                        </ul>
                     </nav>
                 </div>
                 <div className="logo-list"></div>
--- src/Layouts/Home.tsx
+++ src/Layouts/Home.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react';
 import Header from "../Components/Header";
+import OverlayMenu from "../Components/OverlayMenu";
 
 const Home: FC = () => {
     return (
         <>
-         <header>
-             <Header />
-         </header>
-         <h1>Home</h1>
+            <header>
+                <Header/>
+            </header>
+            <h1>Home</h1>
+            <OverlayMenu/>
         </>
     );
 };
